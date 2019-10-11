08876a033ab20779c2b1c052fc6c815eb7cbeefc
Rob Nolan <rlnolan15@gmail.com>
2019-10-11 18:20:32 -0400

    Delete commented out data

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,6 @@ const db = require("../database");
 module.exports = {
   moviePreviews: {
     get: function(callback) {
-      // let queryStr = "Preview.find()";
       db.Preview.find({}, (err, results) => {
         callback(err, results);
       });
