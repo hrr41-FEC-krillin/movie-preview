3df1a779ba0fd4108c4c77d5baf1b9a79a23d3eb
Rob Nolan <rlnolan15@gmail.com>
2019-10-13 15:29:56 -0400

    Check

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class VideoPlayer extends React.Component {
+  render() {
+    return (
+      <div className="aspect-ratio--16x9">
+        <div className="aspect-ratio__inner-wrapper" onclick={this.ModalVideo}>
+          <iframe
+            className="trailer"
+            src={this.props.videoUrl.replace("watch?v=", "embed/")}
+            allowFullScreen
+            frameBorder="0"
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoPlayer;
+
+//
